fa3a557f9dc1e28ba134de93b7d7d0521cf19e7c
David Dada <daveproxy80@gmail.com>
2024-02-15 14:42:41 +0100

    Initial commit

---
+++ test/SaveEther.ts
@@ -0,0 +1,33 @@
+import {loadFixture} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import {expect} from "chai";
+import {ethers} from "hardhat";
+
+describe("Save Ether Contract", () => {
+
+    const deploySaveEther = async () => {
+        const [owner, otherAccounts] = await ethers.getSigners();
+        const SaveEther = await ethers.getContractFactory("SaveEther");
+        const saveEther = await SaveEther.deploy();
+
+        return {saveEther, owner, otherAccounts};
+    }
+
+    describe("Deployment", () => {
+        it('Should deploy', async () => {
+            const {saveEther} = await loadFixture(deploySaveEther);
+
+            expect(saveEther).to.exist;
+        });
+    });
+
+    describe("Deposit", () => {
+        it('Should take in a value before depositing', async () => {
+            const {saveEther, owner} = await loadFixture(deploySaveEther);
+
+            const ownerBalance = await saveEther.checkSavings(owner.address);
+            const deposit = saveEther.deposit;
+            expect(ownerBalance).to.equal((await deposit({value: 1})));
+        });
+    });
+        console.log(deploySaveEther());
+});
